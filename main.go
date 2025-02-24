af15a75ce5c8720f1c593cc16c9c6baabf2f1c6f
ivanlie <ivan.lie@meditap.id>
2025-02-25 00:36:46 +0700

    cors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	controllers "xenditapps/controllers"
@@ -8,6 +9,8 @@ import (
 
 func main() {
 	server := gin.Default()
+
+	server.Use(cors.Default())
 
 	server.GET("/xendit-get-invoice-list", controllers.GetInvoices)
 	server.POST("/xendit-create-invoice", controllers.CreateInvoice)
