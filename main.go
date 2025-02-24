ce1f6372a3e00cca8c1ce4df738e2c9bb7f00a57
ivanlie <ivan.lie@meditap.id>
2025-02-25 00:17:02 +0700

    remove env

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 
 	controllers "xenditapps/controllers"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
 	server := gin.Default()
 
 	server.GET("/xendit-get-invoice-list", controllers.GetInvoices)
